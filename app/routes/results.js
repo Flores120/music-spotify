3620a7bc22d9827fd20754775859411b033ffe51
Megan Kaluza and Tam Nguyen <megan@>
2016-11-07 10:51:24 -0800

    add image to artists

--- app/routes/results.js
+++ app/routes/results.js
@@ -4,10 +4,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function(params) {
 		var key = config.myApiKey;
-   var url = 'https://api.spotify.com/v1/search?q=' + params.search + '&type=artist' + '&apikey=' +key;
-   return Ember.$.getJSON(url).then(function(responseJSON) {
-		 console.log(responseJSON.artists.items[0]);
-		 return responseJSON.artists.items;
+   	var url = 'https://api.spotify.com/v1/search?q=' + params.search + '&type=artist' + '&apikey=' +key;
+   		return Ember.$.getJSON(url).then(function(responseJSON) {
+		 		console.log(responseJSON.artists.items[0]);
+			return responseJSON.artists.items;
    });
  }
 });
