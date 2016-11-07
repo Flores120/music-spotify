46a3c82fced7d66a0f80d134c8948ea055e7cd5b
Alvaro Flores and Nhat Hoang <nhat@>
2016-11-07 14:59:08 -0800

    add play mode.

--- app/routes/details.js
+++ app/routes/details.js
@@ -7,14 +7,19 @@ export default Ember.Route.extend({
     var key = config.myApiKey;
    var url = 'https://api.spotify.com/v1/artists/' + artist.artist_id;
    var url2 = 'https://api.spotify.com/v1/artists/' + artist.artist_id + '/albums';
+   var url3 = 'https://api.spotify.com/v1/artists/' + artist.artist_id + '/top-tracks?country=SE';
 
  return Ember.RSVP.hash({
    artists: Ember.$.getJSON(url).then(function(responseJSON) {
      return responseJSON;
   }),
   albums: Ember.$.getJSON(url2).then(function(responseJSON) {
-    console.log(responseJSON.items);
+    // console.log(responseJSON.items);
     return responseJSON.items;
+  }),
+  tracks: Ember.$.getJSON(url3).then(function(responseJSON) {
+    console.log(responseJSON);
+    return responseJSON.tracks;
   })
 });
 }
