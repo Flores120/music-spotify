1f878f5311e54d7304b2e8e9b5e0433e0e764380
Megan Kaluza and Tam Nguyen <megan+tam@>
2016-11-07 14:32:48 -0800

    add link to images

--- app/routes/details.js
+++ app/routes/details.js
@@ -5,10 +5,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(artist) {
     var key = config.myApiKey;
-   var url = 'https://api.spotify.com/v1/artists/' + artist.artist_id;
-   return Ember.$.getJSON(url).then(function(responseJSON) {
-     console.log(responseJSON);
-     return responseJSON;
+    var url = 'https://api.spotify.com/v1/artists/' + artist.artist_id;
+      return Ember.$.getJSON(url).then(function(responseJSON) {
+      console.log(responseJSON);
+      return responseJSON;
    });
   }
 });
