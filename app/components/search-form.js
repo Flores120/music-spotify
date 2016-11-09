e3e3363b64b6cc262c956292fbf5754b55a37b6e
Megan Kaluza and Tam Nguyen <megan+tam@>
2016-11-09 14:04:51 -0800

    add correct indentation

--- app/components/search-form.js
+++ app/components/search-form.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	actions: {
-	 songLookUp() {
-		 var params = {
-			 search: this.get('search')
-		 };
-		 this.sendAction('songLookUp', params);
-	 }
- }
+		songLookUp() {
+			var params = {
+				search: this.get('search')
+			};
+			this.sendAction('songLookUp', params);
+		}
+	}
 });
