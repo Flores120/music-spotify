ea34003e6d6d10e0f0fb362a5cedcb5f01217d9c
Megan Kaluza <megan.kaluza@gmail.com>
2016-11-10 08:51:17 -0800

    add indentation

--- app/routes/details.js
+++ app/routes/details.js
@@ -1,25 +1,24 @@
 import config from '../config/environment';
 import Ember from 'ember';
 
-
 export default Ember.Route.extend({
   model: function(artist) {
     var key = config.myApiKey;
 
-   var url = 'https://api.spotify.com/v1/artists/' + artist.artist_id;
-   var url2 = 'https://api.spotify.com/v1/artists/' + artist.artist_id + '/albums';
-   var url3 = 'https://api.spotify.com/v1/artists/' + artist.artist_id + '/top-tracks?country=SE';
+    var url = 'https://api.spotify.com/v1/artists/' + artist.artist_id;
+    var url2 = 'https://api.spotify.com/v1/artists/' + artist.artist_id + '/albums';
+    var url3 = 'https://api.spotify.com/v1/artists/' + artist.artist_id + '/top-tracks?country=SE';
 
- return Ember.RSVP.hash({
-   artists: Ember.$.getJSON(url).then(function(responseJSON) {
-     return responseJSON;
-  }),
-  albums: Ember.$.getJSON(url2).then(function(responseJSON) {
-    return responseJSON.items;
-  }),
-  tracks: Ember.$.getJSON(url3).then(function(responseJSON) {
-    return responseJSON.tracks;
-  })
-});
-}
+    return Ember.RSVP.hash({
+      artists: Ember.$.getJSON(url).then(function(responseJSON) {
+        return responseJSON;
+      }),
+      albums: Ember.$.getJSON(url2).then(function(responseJSON) {
+        return responseJSON.items;
+      }),
+      tracks: Ember.$.getJSON(url3).then(function(responseJSON) {
+        return responseJSON.tracks;
+      })
+    });
+  }
 });
